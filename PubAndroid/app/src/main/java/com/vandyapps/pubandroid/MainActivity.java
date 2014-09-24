fe5f0d880149c330014e875c708fc5accab2913b
Mitchell Gordon <mitchell.gordon95@gmail.com>
2014-09-24 00:38:50 -0500

    Resized textures to appropriate sizes, changed the themes a little to get rid of the white peview window on startup.

--- PubAndroid/app/src/main/java/com/vandyapps/pubandroid/MainActivity.java
+++ PubAndroid/app/src/main/java/com/vandyapps/pubandroid/MainActivity.java
@@ -1,11 +1,13 @@
 package com.vandyapps.pubandroid;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 
 
@@ -14,9 +16,6 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Hide the action bar
-        getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
     }
