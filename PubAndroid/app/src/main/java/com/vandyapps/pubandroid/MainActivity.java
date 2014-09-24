121a37cd8cff94f57d855a63623dfabf0baacc41
Mitchell Gordon <mitchell.gordon95@gmail.com>
2014-09-24 02:11:38 -0500

    More main activity styling

--- PubAndroid/app/src/main/java/com/vandyapps/pubandroid/MainActivity.java
+++ PubAndroid/app/src/main/java/com/vandyapps/pubandroid/MainActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import butterknife.OnClick;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -19,6 +21,7 @@ public class MainActivity extends ActionBarActivity {
 
         setContentView(R.layout.activity_main);
     }
+
 
     public void checkOrdersClicked(View v) {
         // Start the order checking activity.
