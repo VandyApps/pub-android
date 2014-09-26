c94687aa0ace941bdc4048af1102b8e1d881476f
Mitchell Gordon <mitchell.gordon95@gmail.com>
2014-09-26 04:17:47 -0500

    Added waiting for multiple orders (buggy)

--- PubAndroid/app/src/main/java/com/vandyapps/pubandroid/OrderActivity.java
+++ PubAndroid/app/src/main/java/com/vandyapps/pubandroid/OrderActivity.java
@@ -40,6 +40,11 @@ public class OrderActivity extends Activity {
     private AtomicBoolean mBound = new AtomicBoolean(false);
     @InjectView(R.id.number_list)
     TextView mTextView;
+    @InjectView(R.id.listview_my_orders)
+    ListView mListView;
+    @InjectView(R.id.edittext_new_order)
+    EditText mEditText;
+    private ArrayAdapter<Integer> mWatching;
     private List<Order> mOrders;
     private QueryService mService;
     private Messenger mMessenger = new Messenger(new PubHandler(this));
@@ -104,8 +109,15 @@ public class OrderActivity extends Activity {
 
         // We have to set our custom font here because apparently
         // there's no way to do it in xml
-        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "dotmatrix.tff");
-        mTextView.setTypeface(myTypeface);
+        try{
+            Typeface myTypeface = Typeface.createFromAsset(getAssets(), "dotmatrix.tff");
+            mTextView.setTypeface(myTypeface);
+        }
+        catch (Exception e) {} // Just eat exceptions. Not having that font is no big deal.
+
+        // Setup the listview & adapter
+        mWatching = new ArrayAdapter<Integer>(this, R.layout.list_item);
+        mListView.setAdapter(mWatching);
 
         Intent i = new Intent(this, QueryService.class);
         bindService(i, mServiceConnection, Context.BIND_AUTO_CREATE);
@@ -118,42 +130,6 @@ public class OrderActivity extends Activity {
         unbindService(mServiceConnection);
     }
 
-    @OnClick(R.id.filter_button)
-    public void notifyClick(View v) {
-        if (!mBound.get())
-            return;
-        final Dialog dlg = new Dialog(this);
-        dlg.setContentView(R.layout.dialog_order_prompt);
-        dlg.setTitle(R.string.order_prompt_text);
-        final EditText et = (EditText) dlg.findViewById(R.id.prompt_et);
-        final Button ok = (Button) dlg.findViewById(R.id.prompt_ok_button);
-        final Button cancel = (Button) dlg
-                .findViewById(R.id.prompt_cancel_button);
-        ok.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                String input = et.getText().toString();
-                try {
-                    int orderNum = Integer.parseInt(input);
-                    mService.setNotify(orderNum);
-                } catch (NumberFormatException e) {
-                    Toast.makeText(OrderActivity.this, "Please enter a number",
-                            Toast.LENGTH_LONG).show();
-                }
-                dlg.dismiss();
-            }
-
-        });
-        cancel.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dlg.dismiss();
-            }
-        });
-        dlg.show();
-    }
-
     private void updateList(List<Order> orders) {
         // Prevent refreshing when there's no new data
         //if (orders.isEmpty() || orders.get(0).getTimeCreated() <= mLastUpdated)
@@ -164,8 +140,33 @@ public class OrderActivity extends Activity {
         mOrders = orders;
         mTextView.setText("");
         for (Order order : orders) {
-            mTextView.append(String.valueOf(order.getOrderNumber()) + "   ");
+            mTextView.append(String.valueOf(order.getOrderNumber()) + " ");
+
+            // TODO - Do something special if we find the order we were looking for.
         }
+    }
+
+    // Called when a user is adding a new order to watch for.
+    @OnClick(R.id.button_add_order)
+    public void addOrder() {
+        int orderNum;
+        try {
+            orderNum = Integer.parseInt(mEditText.getText().toString());
+        } catch (NumberFormatException e) {
+            Toast.makeText(OrderActivity.this, "Please enter a number",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // Add whatever's in the textbox to the list of orders we're watching.
+        mWatching.add(orderNum);
+        mWatching.notifyDataSetChanged();
+
+        // Tell the service we're looking for another order
+        mService.setNotify(orderNum);
+
+        // Clear the edit text
+        mEditText.setText("");
     }
 
 }
--- PubAndroid/app/src/main/java/com/vandyapps/pubandroid/QueryService.java
+++ PubAndroid/app/src/main/java/com/vandyapps/pubandroid/QueryService.java
@@ -1,5 +1,7 @@
 package com.vandyapps.pubandroid;
 
+import java.util.List;
+import java.util.Vector;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -38,9 +40,11 @@ public class QueryService extends Service {
     // Is the activity bound to us?
     private AtomicBoolean mBound = new AtomicBoolean(false);
 
-    // Is there a specific order we're looking for?
-    private AtomicBoolean mNotify = new AtomicBoolean(false);
-    private AtomicInteger mOrderNum = new AtomicInteger();
+    // Are there a specific orders we're looking for?
+    private Vector<Integer> mWatching = new Vector<Integer>();
+
+    // Are we currently running in the foreground?
+    private AtomicBoolean mForeground = new AtomicBoolean(false);
 
     // Used to send messages back to the Activity.
     private Messenger mMessenger;
@@ -76,9 +80,18 @@ public class QueryService extends Service {
                 m.what = Constants.NEW_DATA;
                 m.obj = response.getOrders();
                 mMessenger.send(m);
-                if (mNotify.get()) {
+                if (!mWatching.isEmpty()) {
                     for (Order order : response.getOrders()) {
-                        if (mOrderNum.get() == order.getOrderNumber()) {
+                        if (mWatching.contains(order.getOrderNumber())) {
+                            //Remvoe the order from the watch list
+                            mWatching.remove(new Integer(order.getOrderNumber()));
+
+                            // If there's no more orders we're waiting for, stop running in
+                            // the foreground.
+                            if (mWatching.isEmpty() && mForeground.get()) {
+                                mForeground.set(false);
+                                stopForeground(true);
+                            }
 
                             // Start building the notification.
                             NotificationCompat.Builder b = new NotificationCompat.Builder(
@@ -88,7 +101,7 @@ public class QueryService extends Service {
                             b.setSmallIcon(R.drawable.ic_launcher)
                                     .setTicker("Order ready")
                                     .setContentTitle("Pub")
-                                    .setContentText("Your order is ready");
+                                    .setContentText("Order " + order.getOrderNumber() + " is ready");
 
                             // Make our notification make noise
                             Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
@@ -97,7 +110,6 @@ public class QueryService extends Service {
                             // When they click on the notification, the Activity should open.
                             Intent intent = new Intent(QueryService.this,
                                     OrderActivity.class);
-                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                             b.setContentIntent(getActivity(QueryService.this, 0, intent, 0));
 
                             // When they click on the notification, it should go away.
@@ -124,11 +136,17 @@ public class QueryService extends Service {
     }
 
     /**
-     * This is only called when the user is asking us to stop our background activity.
+     * This is only called when the user is asking us to stop running in the foreground.
      */
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (intent.getAction() == Constants.STOP_ACTION) {
+            // Stop running in the foreground
+            if (mForeground.get()) {
+                mForeground.set(false);
+                stopForeground(true);
+            }
+
             // Stop querying the server
             stopQueries();
 
@@ -155,7 +173,10 @@ public class QueryService extends Service {
 
     @Override
     public boolean onUnbind(Intent intent) {
-        stopQueries();
+        // Only stop the queries if we're not running in the foreground.
+        if (!mForeground.get())
+            stopQueries();
+
         return false;
     }
 
@@ -164,8 +185,37 @@ public class QueryService extends Service {
     }
 
     public void setNotify(int n) {
-        mOrderNum.set(n);
-        mNotify.set(true);
+        // If this is the first time we're called, start running in the foreground.
+        if (mWatching.isEmpty()) {
+            // Make users aware that we are constantly querying in the background.
+
+            // Start building the notification.
+            NotificationCompat.Builder b = new NotificationCompat.Builder(
+                    QueryService.this);
+
+            // Make our notification pretty.
+            b.setSmallIcon(R.drawable.ic_launcher)
+                    .setTicker("Checking Orders...")
+                    .setContentTitle("Checking Orders...")
+                    .setContentText("Click here to stop.");
+
+            // When they click on the notification, we should be flagged to stop everything.
+            Intent intent = new Intent(QueryService.this,
+                    QueryService.class);
+            intent.setAction(Constants.STOP_ACTION);
+            b.setContentIntent(getService(QueryService.this, 0, intent, 0));
+
+            mForeground.set(true);
+            startForeground(1, b.build());
+        }
+
+        // Add the number to the list.
+        mWatching.add(n);
+
+    }
+
+    public List<Integer> getWatching() {
+        return mWatching;
     }
 
     public void startQueries() {
@@ -174,26 +224,6 @@ public class QueryService extends Service {
         }
         mSx.scheduleAtFixedRate(mQueryRunnable, 0, Constants.REFRESH_RATE,
                 TimeUnit.SECONDS);
-
-        // Make users aware that we are constantly querying in the background.
-
-        // Start building the notification.
-        NotificationCompat.Builder b = new NotificationCompat.Builder(
-                QueryService.this);
-
-        // Make our notification pretty.
-        b.setSmallIcon(R.drawable.ic_launcher)
-                .setTicker("Checking Orders...")
-                .setContentTitle("Checking Orders...")
-                .setContentText("Click here to stop.");
-
-        // When they click on the notification, we should be flagged to stop everything.
-        Intent intent = new Intent(QueryService.this,
-                QueryService.class);
-        intent.setAction(Constants.STOP_ACTION);
-        b.setContentIntent(getService(QueryService.this, 0, intent, 0));
-
-        startForeground(1, b.build());
     }
 
     public void stopQueries() {
@@ -201,9 +231,6 @@ public class QueryService extends Service {
             mSx.shutdown();
             mSx = null;
         }
-
-        stopForeground(true);
-        stopSelf();
     }
 
 }
