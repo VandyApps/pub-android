270c8a8f7f59ec1c60f408b42cc3c2443673f42c
Mitchell Gordon <mitchell.gordon95@gmail.com>
2014-12-09 18:24:27 -0600

    Changing url to campus dining

--- PubAndroid/app/src/main/java/com/vandyapps/pubandroid/Constants.java
+++ PubAndroid/app/src/main/java/com/vandyapps/pubandroid/Constants.java
@@ -3,9 +3,9 @@ package com.vandyapps.pubandroid;
 public interface Constants {
 
     int MAX_NUM_ORDERS = 75;
-    String SERVER_ADDRESS = "http://vandyapps.com:7070";
+    String SERVER_ADDRESS = "http://campusdining.vanderbilt.edu:7070";
     int REFRESH_RATE = 15;
-    String API_KEY = "bogus";
+    String API_KEY = "not_implemented";
 
     int NEW_DATA = 1;
     int STOP_REQUEST = 2;
